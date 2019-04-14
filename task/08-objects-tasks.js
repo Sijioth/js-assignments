be0c355200a441778a27c403fe4d31335419a70c
Sijioth <jhnsmthmit@gmail.com>
2019-04-14 20:25:56 +0300

    master/feat: Objects reassigned (-1).

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -119,73 +119,102 @@ function fromJSON(proto, json) {
  *
  *  For more examples see unit tests.
  */
-
 const cssSelectorBuilder = {
-    answer: '',
 
     element: function(value) {
-        this.error(1);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 1;
-        obj.answer = this.answer + value;
-        return obj;
+        throw new Error('Not implemented');
     },
 
     id: function(value) {
-        this.error(2);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 2;
-        obj.answer = this.answer + '#' + value;
-        return obj;
+        throw new Error('Not implemented');
     },
 
     class: function(value) {
-        this.error(3);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 3;
-        obj.answer = this.answer + '.' + value;
-        return obj;
+        throw new Error('Not implemented');
     },
 
     attr: function(value) {
-        this.error(4);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 4;
-        obj.answer = this.answer + '[' + value + ']';
-        return obj;
+        throw new Error('Not implemented');
     },
 
     pseudoClass: function(value) {
-        this.error(5);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 5;
-        obj.answer = this.answer + ':' + value;
-        return obj;
+        throw new Error('Not implemented');
     },
 
     pseudoElement: function(value) {
-        this.error(6);
-        let obj = Object.create(cssSelectorBuilder);
-        obj.i = 6;
-        obj.answer = this.answer + '::' + value;
-        return obj;
+        throw new Error('Not implemented');
     },
 
     combine: function(selector1, combinator, selector2) {
-        let obj = Object.create(cssSelectorBuilder);
-        obj.answer = selector1.answer + ' ' + combinator + ' ' + selector2.answer;
-        return obj;
-    },
-
-    stringify: function() {
-        return this.answer;
-    },
-
-    error: function(newi) {
-        if (this.i > newi) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
-        if (this.i == newi && (newi == 1 || newi == 2 || newi == 6)) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+        throw new Error('Not implemented');
     },
 };
+// const cssSelectorBuilder = {
+//     answer: '',
+
+//     element: function(value) {
+//         this.error(1);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 1;
+//         obj.answer = this.answer + value;
+//         return obj;
+//     },
+
+//     id: function(value) {
+//         this.error(2);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 2;
+//         obj.answer = this.answer + '#' + value;
+//         return obj;
+//     },
+
+//     class: function(value) {
+//         this.error(3);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 3;
+//         obj.answer = this.answer + '.' + value;
+//         return obj;
+//     },
+
+//     attr: function(value) {
+//         this.error(4);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 4;
+//         obj.answer = this.answer + '[' + value + ']';
+//         return obj;
+//     },
+
+//     pseudoClass: function(value) {
+//         this.error(5);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 5;
+//         obj.answer = this.answer + ':' + value;
+//         return obj;
+//     },
+
+//     pseudoElement: function(value) {
+//         this.error(6);
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.i = 6;
+//         obj.answer = this.answer + '::' + value;
+//         return obj;
+//     },
+
+//     combine: function(selector1, combinator, selector2) {
+//         let obj = Object.create(cssSelectorBuilder);
+//         obj.answer = selector1.answer + ' ' + combinator + ' ' + selector2.answer;
+//         return obj;
+//     },
+
+//     stringify: function() {
+//         return this.answer;
+//     },
+
+//     error: function(newi) {
+//         if (this.i > newi) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+//         if (this.i == newi && (newi == 1 || newi == 2 || newi == 6)) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+//     },
+// };
 
 
 module.exports = {
